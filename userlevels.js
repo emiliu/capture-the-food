defd3b0530748556c4ec3dbe184438eec61743a8
emiliu <emilyliu10@gmail.com>
2015-07-25 12:02:25 -0400

    custom level creator and name information

--- userlevels.js
+++ userlevels.js
@@ -14,6 +14,6 @@ function showInfo(data, tabletop) {
 	console.log("tabletop sucessfully processed");
 	console.log(data);
 	for (var i = 0; i < data.length; i++) {
-		$("#levels").append('<a class="btn btn-primary" href="test.html?'+data[i]['board']+'" target="_blank">User Level '+(i+1)+'</a><br/><br/>');
+		$("#levels").append('<a class="btn btn-primary" href="test.html?'+data[i]['board']+'" target="_blank">'+data[i]['title']+" by "+data[i]['creator']+'</a><br/><br/>');
 	}
 }
