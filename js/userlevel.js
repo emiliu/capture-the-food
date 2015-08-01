b377568dc73501e7e98463b8b4ea9a22fc220946
emiliu <emilyliu10@gmail.com>
2015-08-01 13:24:11 -0400

    better represent creator name

--- js/userlevel.js
+++ js/userlevel.js
@@ -15,8 +15,8 @@ function init() {
 function showInfo(data, tabletop) {
 	console.log("tabletop sucessfully processed");
 	console.log(data);
-    $("#level").empty();
-	$("#title").html(data[query.level]["title"]+" by "+data[query.level]["creator"]);
+	$("#title").html(data[query.level]["title"]);
+    $("#level").html(data[query.level]["creator"]);
 	if (isNaN(query.level) || query.level < 0 || query.level >= data.length) {
 		console.log("invalid query");
 		alert("invalid level!");
